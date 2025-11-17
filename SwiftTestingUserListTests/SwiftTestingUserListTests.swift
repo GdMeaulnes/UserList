43fa4e2c675bd46fadebfbec8ec06eef169d0018
Richard DOUXAMI <richard.douxami@gmail.com>
2025-11-17 16:11:27 +0100

    Version finale

---
+++ SwiftTestingUserListTests/SwiftTestingUserListTests.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftTestingUserListTests.swift
+//  SwiftTestingUserListTests
+//
+//  Created by Richard DOUXAMI on 13/11/2025.
+//
+
+import Testing
+
+@Testable import MainViewModel
+
+struct SwiftTestingUserListTests {
+
+    @Test func example() async throws {
+        #expect(MainViewModel().text == "Hello, World!")
+    }
+
+}
